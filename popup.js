41582f00af9fd67d7a41e866bce2702bc1e886c6
mhartz <mshartz5@gmail.com>
2016-10-08 10:29:14 -0400

    scaffolded, initial check working

--- popup.js
+++ popup.js
@@ -1,36 +1,62 @@
-var controls = {
-  
-  messageWithOnlyEmotes: function(this) {
-    $this = this;
-    if () {
-      $(".message-line").each(function() {
-        if ($(this).find('.emoticon').length != 0) {
-          $(this).hide();
-        }
-      }); 
-    }
-  },
-  
-  messageWithEmotes: function() {
-    
-  },
-  
-  emotesByNumber: function() {
-    
-  },
-  
-  emotesOnly: function() { 
-  
-  },
-  
-  repeatedSingleWords: function() {
-    
-  }
-  
-  init: function() {
+//function messageWithOnlyEmotes(dec) {
+//    if (dec.checked) {
+//        console.log("this: " + dec);
+//        $(".message-line").each(function() {
+//            if ($(this).find('.emoticon').length != 0) {
+//                $(this).hide();
+//            }
+//        });
+//    }
+//    else {
+//        $(".message-line").each(function() {
+//            if ($(this).find('.emoticon').length != 0) {
+//                $(this).show();
+//            }
+//        });
+//    }
+//}
+//
+//function messageWithEmotes () {
+//
+//}
+//
+//function messageWithEmotes () {
+//}
+//
+//function emotesByNumber () {
+//}
+//
+//function emotesOnly () {
+//}
+//
+//function repeatedSingleWords () {
+//}
+//
+//$(function() {
+//    $('#message-with-only-emotes').change(function() {
+//        messageWithOnlyEmotes(this);
+//        console.log("clicked");
+//    });
+//});
+
+
+$(function() {
     $('#message-with-only-emotes').change(function() {
-      controls.messageWithOnlyEmotes('#message-with-only-emotes');
+        if ($('#message-with-only-emotes').checked) {
+            console.log("this: " + this);
+            $(".message-line").each(function() {
+                console.log($(this));
+                if ($(this).find('.emoticon').length != 0) {
+                    $(this).hide();
+                }
+            });
+        }
+        else {
+            $(".message-line").each(function() {
+                if ($(this).find('.emoticon').length != 0) {
+                    $(this).show();
+                }
+            });
+        }
     });
-  }
-  
-};
+});
