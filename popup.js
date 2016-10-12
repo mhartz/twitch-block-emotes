cd1f3117abad8d9c724cb46361495de84022ac1e
mhartz <mshartz5@gmail.com>
2016-10-12 00:49:18 -0400

    in page 3 functions working

--- popup.js
+++ popup.js
@@ -1,108 +1,109 @@
-function hideAnyEmotesLines() {
-    $('.message-line').each(function() {
-        if ($(this).find('.emoticon').length != 0) {
-            $(this).hide();
-        }
-    });
+/*
+*	Toggle all emoticons on or off.
+*/
+function toggleAllEmotes() {
+	if ($('#all-emotes').prop('checked')) {
+	    $('.emoticon').each(function() {
+	        $(this).hide();
+	    });
+	}
+	else {
+		$('.emoticon').each(function() {
+	        $(this).show();
+	    });
+	}
 }
-hideAnyEmotesLines();
 
 
-
-function hideAllEmotes() {
-    $('.emoticon').each(function() {
-        $(this).hide();
-    });
+/*
+*	Toggle entire message lines that have any emotes on them to show/hide
+*/
+function toggleMessageWithAnyEmote() {
+	if ($('#message-with-any-emote').prop('checked')) {
+	    $('.message-line').each(function() {
+	        if ($(this).find('.emoticon').length != 0) {
+	            $(this).hide();
+	        }
+	    });
+	}
+	else {
+		$('.message-line').each(function() {
+	        if ($(this).find('.emoticon').length != 0) {
+	            $(this).show();
+	        }
+	    });
+	}
 }
-hideAllEmotes();
 
 
 
-function hideOnlyEmoteLines() {
-   $('.message-line').each(function() {
-       var textMessage = $(this).find('.message').clone().children().remove().end().text().trim();
 
-       if ((textMessage.length <= 0)) {
-           $(this).hide();
-       }
-   });
+/*
+*	Toggle entire messages that only have emoticons on them
+*
+*	Todo: Currently hide user links as well, need to fix
+*/
+function toggleMessageWithOnlyEmote() {
+   if ($('#message-with-only-emote').prop('checked')) {
+	   $('.message-line').each(function() {
+	       var textMessage = $(this).find('.message').clone().children().remove().end().text().trim();
+
+	       if ((textMessage.length <= 0)) {
+	           $(this).hide();
+	       }
+	   });
+	}
+	else {
+		$('.message-line').each(function() {
+	       var textMessage = $(this).find('.message').clone().children().remove().end().text().trim();
+
+	       if ((textMessage.length <= 0)) {
+	           $(this).show();
+	       }
+	   });
+	}
 }
-hideOnlyEmoteLines();
 
 
-
-function hideEmotesByCount(emoticon) {
+/*
+*	Toggle only certain emoticons to show/hide.
+*	Loops through each emoticon that shows and adds them to the selection
+*	so a user can pick and choose which to show/hide.
+*	
+*	ToDo: Loop through active emoticons and add them to the selection, display all items
+*/
+function toggleUsingEmoteName(emoticon) {
 	$('.emoticon[alt="' + emoticon + '"').each(function() {
 		$(this).hide();
 	});
 }
-hideEmotesByCount('qtpFEELS');
+toggleUsingEmoteName('qtpFEELS');
+
+
+/*
+*	Toggle entire message lines that have any more than a specified amount
+*	of emoticons 
+*/
+function toggleMessageWithEmoteCount(emoticon) {
+	
+}
+toggleMessageWithEmoteCount('qtpFEELS');
 
 
 
-
-
-
-
-//function messageWithOnlyEmotes(dec) {
-//    if (dec.checked) {
-//        console.log("this: " + dec);
-//        $(".message-line").each(function() {
-//            if ($(this).find('.emoticon').length != 0) {
-//                $(this).hide();
-//            }
-//        });
-//    }
-//    else {
-//        $(".message-line").each(function() {
-//            if ($(this).find('.emoticon').length != 0) {
-//                $(this).show();
-//            }
-//        });
-//    }
-//}
-//
-//function messageWithEmotes () {
-//
-//}
-//
-//function messageWithEmotes () {
-//}
-//
-//function emotesByNumber () {
-//}
-//
-//function emotesOnly () {
-//}
-//
-//function repeatedSingleWords () {
-//}
-//
-//$(function() {
-//    $('#message-with-only-emotes').change(function() {
-//        messageWithOnlyEmotes(this);
-//        console.log("clicked");
-//    });
-//});
-
-
-//$(function() {
-//    $('#message-with-only-emotes').change(function() {
-//        if ($('#message-with-only-emotes').checked) {
-//            console.log("this: " + this);
-//            $(".message-line").each(function() {
-//                console.log($(this));
-//                if ($(this).find('.emoticon').length != 0) {
-//                    $(this).hide();
-//                }
-//            });
-//        }
-//        else {
-//            $(".message-line").each(function() {
-//                if ($(this).find('.emoticon').length != 0) {
-//                    $(this).show();
-//                }
-//            });
-//        }
-//    });
-//});
+/*
+*	Call all the functions through events
+*/
+$(function() {
+	$('#all-emotes').change(function() {
+		toggleAllEmotes();
+	});
+	
+	$('#message-with-any-emote').change(function() {
+		toggleMessageWithAnyEmote();
+	});
+	
+	$('#message-with-only-emote').change(function() {
+		toggleMessageWithOnlyEmote();
+	});
+});
